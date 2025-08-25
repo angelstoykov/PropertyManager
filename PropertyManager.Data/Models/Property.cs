8ae503d471780d24b4024fd487bdde4bee4543df
Angel Stoykov <angelsto@abv.bg>
2025-08-25 16:17:36 +0300

    added dbcontext, property model

---
+++ PropertyManager.Data/Models/Property.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyManager.Data.Models
+{
+    internal class Property
+    {
+        public required string Name { get; set; }
+        public required string Address { get; set; }
+        public int NumberOfUnits { get; set; }
+        public decimal MonthlyRent { get; set; }
+        public DateTime DateAcquired { get; set; }
+        public required string Owner { get; set; }
+        public required string PropertyType { get; set; }
+        public required string Status { get; set; }
+        public string? Notes { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ PropertyManager.Data/PropertyManagerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PropertyManager.Data.Models;
+
+namespace PropertyManager.Data
+{
+    public class PropertyManagerDbContext : DbContext
+    {
+        public PropertyManagerDbContext(DbContextOptions<PropertyManagerDbContext> options)
+            : base(options)
+        {   
+        }
+
+        DbSet<Property> Properties { get; set; }
+        }
+}
