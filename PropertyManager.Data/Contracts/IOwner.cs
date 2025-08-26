fb0c46247051624bfb155c753da2680b1b8d2eb5
Angel Stoykov <angelsto@abv.bg>
2025-08-26 12:45:00 +0300

    added IOwner interface and Owner data class

---
+++ PropertyManager.Data/Contracts/IOwner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyManager.Data.Contracts
+{
+    internal interface IOwner
+    {
+        public int Id { get; set; }
+        public int OwnerType { get; set; }
+        public string Name { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+        public string BankAccount { get; set; }
+    }
+}
