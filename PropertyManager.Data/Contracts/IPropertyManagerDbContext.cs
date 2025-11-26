a554006564d9569d042cf8e059612862e2efe324
Angel Stoykov <angelsto@abv.bg>
2025-11-26 11:23:59 +0200

    edited db interface

--- PropertyManager.Data/Contracts/IPropertyManagerDbContext.cs
+++ PropertyManager.Data/Contracts/IPropertyManagerDbContext.cs
@@ -5,6 +5,8 @@ namespace PropertyManager.Data.Contracts
 {
     interface IPropertyManagerDbContext
     {
-        public DbSet<Property> Properties { get; set; }
+        public DbSet<Property> Properties { get; }
+
+        public DbSet<Unit> Units { get; }
     }
 }
