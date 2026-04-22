f41631ae523a3b3ca1a9a4c6f379a7cfab2bfb79
Angel Stoykov <angelsto@abv.bg>
2026-04-22 12:46:01 +0300

    added properties to view model

--- PropertyManager.WEB/ViewModels/Properties/PropertyListItemViewModel.cs
+++ PropertyManager.WEB/ViewModels/Properties/PropertyListItemViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace PropertyManager.WEB.ViewModels.Properties
+﻿using PropertyManager.Domain.Models.Enums;
+
+namespace PropertyManager.WEB.ViewModels.Properties
 {
     public class PropertyListItemViewModel
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public string Address { get; set; } = null!;
+        public string Owner { get; set; } = null!;
+        public PropertyType Type { get; set; }
     }
 }
