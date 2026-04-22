902a4c5975a0abe1df36e96db16acacf3992a239
Angel Stoykov <angelsto@abv.bg>
2026-04-22 12:29:43 +0300

    added properties to PropertyListItemDto

--- PropertyManager.Application/DTOs/Properties/PropertyListItemDto.cs
+++ PropertyManager.Application/DTOs/Properties/PropertyListItemDto.cs
@@ -1,9 +1,13 @@
-﻿namespace PropertyManager.Application.DTOs.Properties
+﻿using PropertyManager.Domain.Models.Enums;
+
+namespace PropertyManager.Application.DTOs.Properties
 {
     public class PropertyListItemDto
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public string Address { get; set; } = null!;
+        public string Owner { get; set; } = null!;
+        public PropertyType Type { get; set; }
     }
 }
